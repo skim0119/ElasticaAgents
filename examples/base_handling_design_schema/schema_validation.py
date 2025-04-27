ed0d132f7d4fd3fb2aaaa84eefb555053e963b46
Seung Hyun Kim <skim449@illinois.edu>
2025-04-27 05:06:38 -0500

    update example with rendering

--- examples/base_handling_design_schema/schema_validation.py
+++ examples/base_handling_design_schema/schema_validation.py
@@ -1,5 +1,6 @@
 import json
 from elastica_agents.design_schema import RobotDesignSchema
+from elastica_agents.tool.rendering import render_design
 
 path = "design1.json"
 with open(path, "r") as f:
@@ -7,6 +8,8 @@ with open(path, "r") as f:
 
 try:
     validated_robot = RobotDesignSchema.model_validate(robot_json)
+    render_design(validated_robot)
+
     print("Robot design validation successful!")
     print(
         f"Robot has {len(validated_robot.actuators)} actuators and {len(validated_robot.actuation_groups)} actuation groups"
